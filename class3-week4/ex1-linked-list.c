c44b2792ee019ff7252a0ff12898f4f95f1d63f7
Lara Bjork <lara.m.bjork@gmail.com>
2024-01-28 09:28:57 -0800

    Linked list adds two elements, third element infinite loop

--- class3-week4/ex1-linked-list.c
+++ class3-week4/ex1-linked-list.c
@@ -36,32 +36,34 @@ int main()
   mylist.tail = NULL;
 
   list_add(&mylist, 10);
+  list_dump(&mylist);
   list_add(&mylist, 20);
-  list_add(&mylist, 30);
   list_dump(&mylist);
+  // list_add(&mylist, 30);
+  // list_dump(&mylist);
 
-  mynode = list_find(&mylist, 42);
-  if (mynode == NULL)
-  {
-    printf("Did not find 42\n");
-  }
-  else
-  {
-    printf("Looked for 42, found %d\n", mynode->value);
-  }
+  // mynode = list_find(&mylist, 42);
+  // if (mynode == NULL)
+  // {
+  //   printf("Did not find 42\n");
+  // }
+  // else
+  // {
+  //   printf("Looked for 42, found %d\n", mynode->value);
+  // }
 
-  mynode = list_find(&mylist, 30);
-  if (mynode == NULL || mynode->value != 30)
-  {
-    printf("Did not find 30\n");
-  }
-  else
-  {
-    printf("Found 30\n");
-  }
+  // mynode = list_find(&mylist, 30);
+  // if (mynode == NULL || mynode->value != 30)
+  // {
+  //   printf("Did not find 30\n");
+  // }
+  // else
+  // {
+  //   printf("Found 30\n");
+  // }
 
-  list_add(&mylist, 40);
-  list_dump(&mylist);
+  // list_add(&mylist, 40);
+  // list_dump(&mylist);
 }
 
 void list_dump(lst) struct list *lst;
@@ -78,8 +80,42 @@ void list_dump(lst) struct list *lst;
 void list_add(lst, value) struct list *lst;
 int value;
 {
+  while (1)
+  {
+    if (lst->head == NULL)
+    {
+      printf("part 1\n");
+      struct lnode *new_node = (struct lnode *)
+          malloc(sizeof(struct lnode));
+      new_node->value = value;
+      new_node->next = NULL;
+      lst->head = new_node;
+      return;
+    }
+    else if (lst->head->next == NULL)
+    {
+      printf("part2\n");
+      struct lnode *new_node = (struct lnode *)
+          malloc(sizeof(struct lnode));
+      new_node->value = value;
+      new_node->next = NULL;
+      lst->head->next = new_node;
+      lst->tail = new_node;
+      return;
+    }
+    else
+    {
+      printf("part 3\n");
+      printf("lst.head.value: %d", lst->head->value);
+      struct list *new_list = (struct list *)
+          malloc(sizeof(struct list));
+      new_list->head = lst->head->next;
+      new_list->tail = lst->tail;
+      list_add(new_list, value);
+    }
+  }
+
   /* Append the value to the end of the linked list. */
-  
 }
 
 struct lnode *list_find(lst, value)
