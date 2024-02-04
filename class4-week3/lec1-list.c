9e9dc4fe81b25a6a7a59336ec019ebee4bce249e
Lara Bjork <lara.m.bjork@gmail.com>
2024-02-04 13:11:19 -0800

    lec 1 list

---
+++ class4-week3/lec1-list.c
@@ -0,0 +1,44 @@
+
+#include <stdlib.h>
+
+struct lnode
+{
+  char *text;
+  struct lnode *next;
+};
+
+struct pylist
+{
+  struct lnode *head;
+  struct lnode *tail;
+  int count;
+};
+
+/* Constructor: lst = list() */
+struct pylist *pylist_new()
+{
+  struct pylist *p = malloc(sizeof(*p));
+  p->head = NULL;
+  p->tail = NULL;
+  p->count = 0;
+  return p;
+};
+
+/* Destructor: del(lst) */
+void pylist_del(struct pylist *self)
+{
+  struct lnode *cur, *next;
+  cur = self->head;
+  while (cur)
+  {
+    free(cur->text);
+    next = cur->next;
+    free(cur);
+    cur = next;
+  }
+  free((void *)self);
+}
+
+int main()
+{
+}
