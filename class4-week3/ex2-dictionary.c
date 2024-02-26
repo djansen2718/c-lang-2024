803fef7942435adeba1c45232ea47daf8643d912
Lara Bjork <lara.m.bjork@gmail.com>
2024-02-25 17:08:10 -0800

    start ex2 class4 week3

---
+++ class4-week3/ex2-dictionary.c
@@ -0,0 +1,136 @@
+/* EXPECTED OUTPUT */
+/*
+{'z': 'Catch phrase'}
+{'z': 'W'}
+{'z': 'W', 'y': 'B', 'c': 'C', 'a': 'D'}
+Length =4
+z=W
+x=(null)
+
+Dump
+z=W
+y=B
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct dnode
+{
+  char *key;
+  char *value;
+  struct dnode *next;
+};
+
+struct pydict
+{
+  struct dnode *head;
+  struct dnode *tail;
+  int count;
+};
+
+/* Constructor - dct = dict() */
+struct pydict *pydict_new()
+{
+  struct pydict *p = malloc(sizeof(*p));
+  p->head = NULL;
+  p->tail = NULL;
+  p->count = 0;
+  return p;
+}
+
+/* Destructor - del(dct) */
+void pydict_del(struct pydict *self)
+{
+  struct dnode *cur, *next;
+  cur = self->head;
+  while (cur)
+  {
+    free(cur->key);
+    free(cur->value);
+    next = cur->next;
+    free(cur);
+    cur = next;
+  }
+  free((void *)self);
+}
+
+int main(void)
+{
+  struct dnode *cur;
+  struct pydict *dct = pydict_new();
+
+  setvbuf(stdout, NULL, _IONBF, 0); /* Internal */
+
+  pydict_put(dct, "z", "Catch phrase");
+  pydict_print(dct);
+  pydict_put(dct, "z", "W");
+  pydict_print(dct);
+  pydict_put(dct, "y", "B");
+  pydict_put(dct, "c", "C");
+  pydict_put(dct, "a", "D");
+  pydict_print(dct);
+  printf("Length =%d\n", pydict_len(dct));
+
+  printf("z=%s\n", pydict_get(dct, "z"));
+  printf("x=%s\n", pydict_get(dct, "x"));
+
+  printf("\nDump\n");
+  for (cur = dct->head; cur != NULL; cur = cur->next)
+  {
+    printf("%s=%s\n", cur->key, cur->value);
+  }
+
+  pydict_del(dct);
+}
+
+/* EXERCISE CODE STARTS HERE*/
+/* print(dct) */
+/* {'z': 'W', 'y': 'B', 'c': 'C', 'a': 'D'} */
+void pydict_print(struct pydict *self)
+{
+  struct dnode *cur, *next;
+  cur = self->head;
+  printf("{'%s': '%s'", cur->key, cur->value);
+  cur = cur->next;
+  while (cur)
+  {
+    printf(", '%s': '%s'", cur->key, cur->value);
+    cur = cur->next;
+  }
+  printf("}");
+}
+
+int pydict_len(const struct pydict *self)
+{
+  return self->count;
+}
+
+/* find a node - used in get and put */
+struct dnode *pydict_find(struct pydict *self, char *key)
+{
+  return NULL;
+}
+
+/* x.get(key) - Returns NULL if not found */
+char *pydict_get(struct pydict *self, char *key)
+{
+  return NULL;
+}
+
+/* x[key] = value; Insert or replace the value associated with a key */
+void pydict_put(struct pydict *self, char *key, char *value)
+{
+  struct dnode *cur = self->head;
+  while (cur)
+  {
+    if (cur->key == key)
+    {
+      free(cur->value);
+      cur->value = malloc(sizeof(cur->value));
+      cur->value = value;
+    }
+  }
+}
+/* EXERCISE CODE ENDS HERE*/
